80e92a965688505c878c737bdc915b3764e42284
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2026-03-30 15:00:41 +0200

    read arg parse

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+import yaml
+import argparse
+
 def main():
     print("Hello from graph-rag!")
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', default='config.yaml')
+
+    args = parser.parse_args()
+
+    with open(args.config) as f:
+        cfg = yaml.safe_load(f)
+
     main()
