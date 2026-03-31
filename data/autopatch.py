73eba3c0a929ad5d7a0ce4f6650d19bc7859c3e9
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2026-03-31 08:58:41 +0200

    format autopatch name

--- data/autopatch.py
+++ data/autopatch.py
@@ -1,11 +1,18 @@
 from typing import Iterator
-
+import glob
+import os
 from .base import BaseDataset, FunctionPair
 
 class AutoPatchDataset(BaseDataset):
     def name(self) -> str:
-        return "autopatch"
+        return "AutoPatch"
     
+    def _load_files(self):
+        datset_path = self.config["path"]
+        glob.glob(
+        os.path.join(datset_path, "**", "export.xml"), recursive=True
+    )
+
     def stream(self) -> Iterator[FunctionPair]:
 
         ...
