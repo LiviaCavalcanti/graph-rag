d025fb49dba17fabec81968315b34e47fefe348a
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2026-03-31 08:54:21 +0200

    instatiate dataset class

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import yaml
 import argparse
 
-def main():
+DATASETS = ["autopatch", 'cvefixes']
+
+def main(cfg):
+    active_datasets = [name for name in DATASETS if cfg['data'].get(name)]
+    for ds_name in active_datasets:
+        ds_cfg = cfg['data'][ds_name]
+        # instantiate the dataset class with the config params
+        dataset = DATASETS[ds_name](ds_cfg)
     print("Hello from graph-rag!")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', default='config.yaml')
+    parser.add_argument('--mode', choices=['index', 'query'], default='index')
 
     args = parser.parse_args()
 
     with open(args.config) as f:
         cfg = yaml.safe_load(f)
 
-    main()
+    main(cfg)
