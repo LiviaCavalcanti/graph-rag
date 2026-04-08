4c69135f9bd9a78cf4a366e05fc6275d4037e5cf
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2026-04-08 15:37:53 +0200

    add new embeddings instantiation

--- src/embeddings/__init__.py
+++ src/embeddings/__init__.py
@@ -1,7 +1,13 @@
 from .netlsd import NetLSDEmbedder
+from .wl import WLEmbedder
+from .gin import GINEmbedder
+from .combined import CombinedEmbedder
 
 REGISTRY: dict[str, type] = {
     "netlsd": NetLSDEmbedder,
+    "wl": WLEmbedder,
+    "gin": GINEmbedder,
+    "combined": CombinedEmbedder,
 }
 
 
