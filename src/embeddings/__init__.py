91d6daaa71b218170da4be6007b8d5a45b9e919d
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2026-04-15 17:26:54 +0200

    add RGCN

--- src/embeddings/__init__.py
+++ src/embeddings/__init__.py
@@ -1,13 +1,18 @@
+from .motif import MotifEmbedder
+
 from .netlsd import NetLSDEmbedder
 from .wl import WLEmbedder
 from .gin import GINEmbedder
 from .combined import CombinedEmbedder
+from .rgcn import RGCNEmbedder
 
 REGISTRY: dict[str, type] = {
     "netlsd": NetLSDEmbedder,
     "wl": WLEmbedder,
     "gin": GINEmbedder,
     "combined": CombinedEmbedder,
+    'motif':    MotifEmbedder,
+    'rgcn':  RGCNEmbedder,
 }
 
 
