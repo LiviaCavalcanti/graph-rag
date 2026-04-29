cdbca39bf56b1f224370715831abb537047c22c3
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2026-04-29 11:43:01 +0200

    implement sharable read dataset files

--- src/io/__init__.py
+++ src/io/__init__.py
@@ -3,8 +3,10 @@
 from src.io.results import (
     BackgroundWriter,
     append_jsonl,
+    find_cve_dir,
     load_completed,
     load_db_cache,
+    load_ground_truth,
     read_code_file,
     save_json,
 )
@@ -12,8 +14,10 @@ from src.io.results import (
 __all__ = [
     "BackgroundWriter",
     "append_jsonl",
+    "find_cve_dir",
     "load_completed",
     "load_db_cache",
+    "load_ground_truth",
     "read_code_file",
     "save_json",
 ]
