69f06ee5bf667768198512de9aed27d32f501ef5
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2026-05-12 17:18:29 +0200

    remove references to dead code

--- experiments/__init__.py
+++
@@ -1,9 +0,0 @@
-from .runner import run_experiment
-from .common import (
-    load_pairs,  # re-exported from src.data.autopatch
-    build_split,
-    build_hnsw,
-    evaluate_retrieval,
-    evaluate_cwe_recall,
-    save_json,
-)
