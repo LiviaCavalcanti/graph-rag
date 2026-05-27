e8cc801c6933d28ffb2b8c804a9ab58d5201c408
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2026-05-27 10:10:52 +0200

    import embeddings variations

--- src/embeddings/__init__.py
+++ src/embeddings/__init__.py
@@ -1,23 +1,27 @@
-from .codebert_seq import CodeBERTSeqEmbedder
+from .codebert_seq import CodeBERTFlowEmbedder, CodeBERTSeqEmbedder
 from .codexglue_baseline import CodeXGLUEBaselineEmbedder
-from .combined import CombinedEmbedder
-from .gin import GINEmbedder
+from .combined import CombinedEmbedder, CombinedEnrichedEmbedder
+from .gin import GINEmbedder, GINEnrichedEmbedder
 from .motif import MotifEmbedder
 from .netlsd import NetLSDEmbedder
 from .rgcn import RGCNEmbedder
-from .vuln_pattern import CodeBERTPatternEmbedder, VulnPatternEmbedder
+from .vuln_pattern import CodeBERTFlowPatternEmbedder, CodeBERTPatternEmbedder, VulnPatternEmbedder
 from .wl import WLEmbedder
 
 REGISTRY: dict[str, type] = {
     "netlsd": NetLSDEmbedder,
     "wl": WLEmbedder,
     "gin": GINEmbedder,
+    "gin_enriched": GINEnrichedEmbedder,
     "combined": CombinedEmbedder,
+    "combined_enriched": CombinedEnrichedEmbedder,
     "motif": MotifEmbedder,
     "rgcn": RGCNEmbedder,
     "codebert_seq": CodeBERTSeqEmbedder,
     "vuln_pattern": VulnPatternEmbedder,
     "codebert_pattern": CodeBERTPatternEmbedder,
+    "codebert_flow": CodeBERTFlowEmbedder,
+    "codebert_flow_pattern": CodeBERTFlowPatternEmbedder,
     "codexglue_baseline": CodeXGLUEBaselineEmbedder,
 }
 
